b977e35a2f531ea3de85ae4736d67ed195a2135e
0400h <github@0400h.cn>
2019-06-26 10:22:10 +0800

    fix MatMul transpose bug

--- matmul.py
+++ matmul.py
@@ -7,13 +7,13 @@ def MatMul(mat_a, mat_b, mat_out):
     shape_m = mat_a.shape[0]
     shape_k = mat_a.shape[1]
     shape_n = mat_b.shape[1]
-    mat_b = mat_b.reshape(shape_n, shape_k)
+    mat_b_trans = np.transpose(mat_b)
     for m_index in range(shape_m):
         for n_index in range(shape_n):
             for k_index in range(shape_k):
                 mat_out[m_index, n_index] += mat_a[m_index, k_index] \
-                                           * mat_b[n_index, k_index]
-    # print(mat_a.shape, mat_b.shape, mat_out.shape, mat_out)
+                                           * mat_b_trans[n_index, k_index]
+    # print('MatMul Verbose:', mat_a[0], mat_b_trans, mat_out)
     return None
 
 def MatMul_SnSn(mat_a, mat_b, mat_out):
@@ -43,8 +43,9 @@ def MatMul_SnSn(mat_a, mat_b, mat_out):
         mat_a = mat_a + 2**(input_bits-1)
         mat_a = mat_a.astype(eval('np.uint'+str(input_bits)))
         mat_out_offset = np.zeros((1, shape_n), dtype=eval('np.int'+str(output_bits)))
-        for index in range(shape_n):
-            mat_out_offset[0, index] = sum(mat_b[:, index])
+        for n_index in range(shape_n):
+            mat_out_offset[0, n_index] = np.sum(mat_b[:, n_index])
+        # print('mat_out_offset', mat_out_offset.dtype, mat_out_offset)
         mat_out_offset = -2**(input_bits-1) * mat_out_offset
         mat_out_offset = np.tile(mat_out_offset, (shape_m, 1))
     else:
@@ -53,9 +54,11 @@ def MatMul_SnSn(mat_a, mat_b, mat_out):
         mat_out_offset = np.zeros((shape_m, 1), dtype=eval('np.int'+str(output_bits)))
         for index in range(shape_m):
             mat_out_offset[index, 0] = sum(mat_a[index, :])
+        # print('mat_out_offset', mat_out_offset.dtype, mat_out_offset)
         mat_out_offset = -2**(input_bits-1) * mat_out_offset
         mat_out_offset = np.tile(mat_out_offset, (1, shape_n))
 
+    # print('mat_out_offset', mat_out_offset.dtype, mat_out_offset)
     MatMul(mat_a, mat_b, mat_out)
     mat_out += mat_out_offset
     return None
@@ -65,18 +68,21 @@ def matmul_test(test_case, bits):
     mat_un = np.full((dim_m, dim_k), value_a, dtype=eval('np.uint'+str(bits)))
     mat_sn_1 = np.full((dim_m, dim_k), value_a, dtype=eval('np.int'+str(bits)))
     mat_sn_2 = np.full((dim_k, dim_n), value_b, dtype=eval('np.int'+str(bits)))
+    # mat_sn_2 = np.random.randint(-2**(bits-1), 2**(bits-1), size=(dim_k, dim_n), dtype=eval('np.int'+str(bits)))
+    # mat_sn_2 = np.random.randint(0, 2, size=(dim_k, dim_n), dtype=eval('np.int'+str(bits)))
     mat_out_ref = np.zeros((dim_m, dim_n), dtype=np.int64)
     mat_out = np.zeros((dim_m, dim_n), dtype=np.int64)
     MatMul(mat_un, mat_sn_2, mat_out_ref)
     MatMul_SnSn(mat_sn_1, mat_sn_2, mat_out)
     count, length = matrix_validation(mat_out_ref, mat_out)
-    # print('mat_out_ref', mat_out_ref.dtype, mat_out_ref)
-    # print('mat_out', mat_out.dtype, mat_out)
+    # print('mat_out_ref', mat_out_ref.dtype, mat_out_ref[0])
+    # print('mat_out', mat_out.dtype, mat_out[0])
     return 1.0 * count / length
 
 class unit_test(unittest.TestCase):
     target_error_rate = 0.0
     test_case = [
+        (1, 1, 32, 64, 64),
         (128, 64, 1, 0, 0),
         (128, 64, 32, 0, 0),
         (128, 64, 32, 0, 64),
@@ -84,9 +90,8 @@ class unit_test(unittest.TestCase):
         (128, 64, 32, 64, 64),
         (128, 64, 32, 64, -64),
         (128, 64, 32, 127, -128),
+        (128, 64, 512, 127, -128),
         (64, 128, 32, 127, -128),
-        (64, 128, 128, 127, -128),
-        (64, 128, 256, 127, -128),
         (64, 128, 512, 127, -128),
     ]
 
